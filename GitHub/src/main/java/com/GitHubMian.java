fc829e5721c825421f422e1f29ee7ca25c36a1af
tianzhongping <13854180785@163.com>
2021-05-19 22:54:20 +0800

    Update GitHubMian.java
    hhh

--- GitHub/src/main/java/com/GitHubMian.java
+++ GitHub/src/main/java/com/GitHubMian.java
@@ -3,7 +3,7 @@ package com;
 public class GitHubMian {
     public static void main(String[] args) {
         System.out.println("MMMMMHHH");
-        System.out.println("aaaaaa");
-        System.out.println("yyyyyy");
+        System.out.println("aaaaassa");
+        System.out.println("yyyyyssy");
     }
 }
